4eafa2a494fffc585519bb997b0abf6886c63423
asbarrientos <98504001+asbarrientos@users.noreply.github.com>
2022-02-10 11:53:12 +0800

    Add files via upload

---
+++ Prelims-Demo-1/Server/serverr.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server =  http.createServer(function(req, res){
+
+    // path code here
+
+});
+
+server.listen(2500);
+console.log('NodeJS localhost at port 2500 is running')
