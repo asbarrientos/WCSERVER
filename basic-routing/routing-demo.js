c0e1a6ec93fb582214337e2f9837e468d0de2607
asbarrientos <98504001+asbarrientos@users.noreply.github.com>
2022-03-03 00:04:44 +0800

    Add files via upload

---
+++ basic-routing/routing-demo.js
@@ -0,0 +1,37 @@
+// Name: Barrientos, Abigail S
+// App Name: routing-demo.js
+// Description: This program uses BASIC ROUTING and the result shows different requests in plain text format.
+
+var express = require("Express");
+var app = express();
+
+//This responds with "Hello World" on the homepage.
+app.get('/', function(request, response){
+    console.log("There is GET request for the homepage!");
+    response.send('Here is the GET Method!');
+});
+
+//This responds with a POST request for the homepage.
+app.get('/post', function(request, response){
+    console.log("A POST request for the homepage is accessed.");
+    response.send('Here is the POST Method!');
+});
+
+//This responds with a GET request for the /list_user page.
+app.get('/list_user', function(request, response){
+    console.log("Got a GET request for /list_user.");
+    response.send('This is Page Listing');
+});
+
+//This responds with a GET request for abcd, abxcd, ab123cd, and so on.
+app.get('/ab*cd', function(request, response){
+    console.log("Got a GET request for /ab*cd.");
+    response.send('Pattern Match Page');
+});
+
+var server = app.listen(0926, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port);
+});
